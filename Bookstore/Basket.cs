53ebf2756b14138d7540812c0461b996c5616a98
Stephen Smith <stephen.smith@zeninternet.co.uk>
2020-03-21 11:46:05 +0000

    Refactored basket total calculation

--- Bookstore/Basket.cs
+++ Bookstore/Basket.cs
@@ -11,49 +11,47 @@ namespace Bookstore
         }
 
         public IList<IItem> Items { get; }
-        public decimal Total
+        public decimal Total => BookSubTotal + OtherItemSubTotal;
+
+        private decimal OtherItemSubTotal => Items.Where(item => !(item is Book)).Sum(item => item.Price);
+
+        private decimal BookSubTotal
         {
             get
             {
-                var total = 0M;
-
-                var bookBundles = new List<HashSet<Book>>();
+                var books = new List<Book>();
 
                 foreach (var item in Items)
                 {
-                    if (!(item is Book book)) //item is not a book}
-                    {
-                        total += item.Price;
-                        continue;
-                    }
+                    if (item is Book book)
+                        books.Add(book);
+                }
 
+                var subTotal = 0M;
+
+                var bookBundles = new List<HashSet<Book>>();
+
+                foreach (var book in books)
+                {
                     var added = false;
 
                     foreach (var bookBundle in bookBundles.Where(bookBundle => bookBundle.Count < 2))
-                    {
                         added = bookBundle.Add(book);
-                    }
 
                     if (!added) //Start a new bundle
-                    {
                         bookBundles.Add(new HashSet<Book>() { book });
-                    }
 
                 }
 
                 foreach (var bookBundle in bookBundles)
                 {
                     if (bookBundle.Count == 2)
-                    {
-                        total += bookBundle.Sum(book => book.Price) * .95M;
-                    }
+                        subTotal += bookBundle.Sum(book => book.Price) * .95M;
                     else
-                    {
-                        total += bookBundle.Sum(book => book.Price);
-                    }
+                        subTotal += bookBundle.Sum(book => book.Price);
                 }
 
-                return total;
+                return subTotal;
             }
         }
 
