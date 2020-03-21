53516c1319b5aaf8bb150e31762c81245fa6327c
Stephen Smith <stephen.smith@zeninternet.co.uk>
2020-03-21 14:08:22 +0000

    Added test for bundle of three books and additional book as this was also mentioned in kata breif

--- UnitTests/MainScenarioTests.cs
+++ UnitTests/MainScenarioTests.cs
@@ -10,19 +10,34 @@ namespace UnitTests
     [TestClass]
     public class MainScenarioTests
     {
+        private Dictionary<int, decimal> _bookBundleRules = new Dictionary<int, decimal>()
+        {
+            {2, 0.05M},
+            {3, 0.1M},
+            {4, 0.2M},
+            {5, 0.25M},
+        };
+
+        [TestMethod]
+        public void Basket_With_A_Bundle_Of_Three_Books_And_An_Additional_Book_Costs_29_Eur_60_Cents()
+        {
+            //Arrange
+            var basket = new Basket(_bookBundleRules);
+
+            //Act
+            basket.Add(item: new Book(code: 1), qty: 2);
+            basket.Add(item: new Book(code: 2));
+            basket.Add(item: new Book(code: 3));
+
+            //Assert
+            Assert.AreEqual(expected: 29.6M, actual: basket.Total);
+        }
+
         [TestMethod]
         public void Basket_With_A_Bundle_Of_Five_Books_And_A_Bundle_Of_Three_Books_Costs_51_Eur_60_Cents()
         {
             //Arrange
-            var bookBundleRules = new Dictionary<int, decimal>()
-            {
-                {2, 0.05M},
-                {3, 0.1M},
-                {4, 0.2M},
-                {5, 0.25M},
-            };
-
-            var basket = new Basket(bookBundleRules);
+            var basket = new Basket(_bookBundleRules);
 
             //Act
             basket.Add(item: new Book(code: 1), qty: 2);
